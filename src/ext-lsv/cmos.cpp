17df241ed3dbfb731d6ae5f20fb451800d333a27
geneyao0302 <geneyao0302@gmail.com>
2020-12-27 10:48:41 +0800

    clean up code and print all path (Node)

--- src/ext-lsv/cmos.cpp
+++ src/ext-lsv/cmos.cpp
@@ -68,7 +68,7 @@ int CommandCmos2Sop(Abc_Frame_t* pAbc, int argc, char** argv)
     if (*argv[1] == 'n') isNmos = true;
 
     Graph mos_net(argv[2]);
-    mos_net.dump();
+    //mos_net.dump();
 
     Cmos2Sop(&mos_net, isNmos);
 
@@ -91,6 +91,16 @@ void Cmos2Sop(Graph* mos_net, bool isNmos)
     Search(s, t, mos_net ,&all_path, &path, &seen);
 
     // Turn all path into boolean expression
+    std::cout << "----print all path ----\n";
+    int i = 0;
+    for (auto p: all_path) {
+        std::cout << "path " << i << ": ";
+        for (Node *n: p) {
+            std::cout << n->idx << " ";
+        }
+        std::cout << std::endl;
+        i++;
+    }
 
     // dump blif
     
@@ -98,12 +108,10 @@ void Cmos2Sop(Graph* mos_net, bool isNmos)
 
 void Search(Node* x, Node * t, Graph* mos_net ,std::vector<std::vector<Node*>>* all_path, std::vector<Node*>* path, std::vector<Node*>* seen)
 {
-    //char temp[256];
-    //path->push_back(x);
     if (x->idx == t->idx) {
-        std::cout << "----terminate----\n";
+        //std::cout << "----terminate----\n";
         all_path->push_back(*path);
-        printPath(*path);
+        //printPath(*path);
         return;
     }
     
@@ -111,24 +119,13 @@ void Search(Node* x, Node * t, Graph* mos_net ,std::vector<std::vector<Node*>>* 
     for (Node *n:*path) {
         seen->push_back(n);
     }
-    //printf("Seen: ");
-    //printPath(*seen);
 
     if (Stuck(x, t, mos_net, seen)) return;
-    //std::cout << "----test----\n";
     for (Node *n: x->neighbors) {
-        //std::cout << "x neighbor: " << n->idx << std::endl;
         if (std::count(path->begin(), path->end(), n) == 0) {
             path->push_back(n);
-            //std::cout << "----Search Begin----\n";
-            //printPath(*path);
             Search(n, t, mos_net , all_path, path, seen);
-            //std::cin.get(temp,256);
-            //std::cout << "----Search End----\n";
-            //printPath(*path);
             path->pop_back();
-            //printPath(*path);
-            //std::cin >> temp;
         }
     }
 
@@ -136,19 +133,12 @@ void Search(Node* x, Node * t, Graph* mos_net ,std::vector<std::vector<Node*>>* 
 
 bool Stuck(Node* x, Node * t, Graph* mos_net , std::vector<Node*>* seen)
 {
-    //printf("-----stuck---\n");
-    //std::cout << "stuck n: " << x->idx << std::endl; 
     if (x->idx == t->idx) return false;
-    //printf("Seen: ");
-    //printPath(*seen);
-
+    
     for (Node* n:x->neighbors) {
         bool isSeen = false;
-        //std::cout << "x neighbor: " << n->idx << std::endl;
         for (Node *s:*seen) {
-            //std::cout << "s: " << s->idx << ", n: " << n->idx << std::endl;
             if (s->idx == n->idx) {
-                //printf("QQ\n");
                 isSeen = true;
                 break;
             }
